4ecc15db67ce0260cf70be7e8a0b26b12ff329b5
CheckIT-App <30834961+CheckIT-App@users.noreply.github.com>
2017-08-08 23:56:55 +0300

    pages added

---
+++ src/pages/newCheck/newCheck.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-newCheck',
+  templateUrl: 'newCheck.html'
+})
+export class NewCheckPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
