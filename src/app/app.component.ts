1256f74db5e2c72faa4fb11c9fcd5abc7d905fd9
bracha <bracha@irox.net>
2017-08-17 12:22:15 +0300

    firebase added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,31 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import firebase from 'firebase';
 import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
-
+ 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+     firebase.initializeApp({
+     apiKey: "AIzaSyAMKNoiaRQFFmPRttWwaEiYn82_9S24xyY",
+    authDomain: "checkit-f199c.firebaseapp.com",
+    databaseURL: "https://checkit-f199c.firebaseio.com",
+    projectId: "checkit-f199c",
+    storageBucket: "checkit-f199c.appspot.com",
+    messagingSenderId: "55755262892"
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+
+    
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -9,6 +11,21 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  trySave(){
+    console.log("enter to func");
+    //var defaultDatabase = firebase.database();
+//     var messageListRef = firebase.database().ref('users');
+// var newMessageRef = messageListRef.push();
+// newMessageRef.set({
+//   'user_id': 'ada',
+//   'text': 'The Analytical Engine weaves algebraical patterns just as the Jacquard loom weaves flowers and leaves.'
+// });
+
+    firebase.database().ref().push().set({
+      "name":"a",
+      "gmail address":"a@a.com"
+    })
   }
 
 }
